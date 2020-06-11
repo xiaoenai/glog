4d00978225a752b717de90e05776742419867388
胡杨 <chuang.xie@enai.im>
2020-06-11 22:22:07 +0800

    add example

---
+++ example/example.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/xiaoenai/glog"
+	"time"
+)
+
+func main(){
+	glog.DefaultLogger().Level =glog.NewLevel("TRACE")
+	glog.Tracef("TRACE-> %d",time.Now().Unix())
+	glog.Debugf("DEBUG")
+	glog.Infof("INFO")
+	glog.Warnf("WARN")
+	glog.Errorf("ERROR")
+	glog.Fatalf("FATALF")
+}
+
+
